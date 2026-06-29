e059c55ef3f7901d9d10143229ca56643a4f1df4
Kathirvel1018 <ganesankathir1018@gmail.com>
2026-06-29 14:44:33 +0530

    solved Routes

--- frontend/src/app/rootReducer.js
+++ frontend/src/app/rootReducer.js
@@ -1,8 +1,7 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import authReducer from '../modules/auth/authSlice';
 import dashboardReducer from '../modules/dashboard/dashboardSlice';
 import tenantReducer from '../modules/tenant/tenantSlice';
-import patientReducer from '../modules/patients/patientSlice';
 import patientReducer from '../modules/patients/patientSlice';
 import staffReducer from '../modules/staff/staffSlice';
 
--- frontend/src/app/rootSaga.js
+++ frontend/src/app/rootSaga.js
@@ -2,15 +2,9 @@ import { all } from 'redux-saga/effects';
 import authSaga from '../modules/auth/authSaga';
 import dashboardSaga from '../modules/dashboard/dashboardSaga';
 import tenantSaga from '../modules/tenant/tenantSaga';
-
-import patientSaga from '../modules/patients/patientSaga';
 import staffSaga from '../modules/staff/staffSaga';
-
-
-
 import patientSaga from '../modules/patients/patientSaga';
 
-import staffSaga from '../modules/staff/staffSaga';
 
 
 export default function* rootSaga() {
